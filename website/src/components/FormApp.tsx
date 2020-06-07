d1bd64a1e3343016c99370d1f0d1f92f1aa20891
Sreeni <sreeni@cloudio.io>
2020-06-07 16:22:04 -0700

    mui theme to match the L&F

--- website/src/components/FormApp.tsx
+++ website/src/components/FormApp.tsx
@@ -2,11 +2,43 @@ import React from 'react';
 import { StateXProvider } from '@cloudio/statex';
 import Form from './Form';
 import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: 'rgba(239, 103, 13, 0.65)',
+      main: '#ef670d',
+      dark: '#ef670d',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: 'rgba(239, 103, 13, 0.65)',
+      main: '#ef670d',
+      dark: '#ef670d',
+      contrastText: '#fff',
+    },
+  },
+  overrides: {
+    MuiButton: {
+      root: {
+        textTransform: 'none',
+      },
+    },
+    MuiTab: {
+      root: {
+        textTransform: 'none',
+      },
+    },
+  },
+});
 
 export default function App() {
   return ExecutionEnvironment.canUseDOM ? (
     <StateXProvider>
-      <Form />
+      <ThemeProvider theme={theme}>
+        <Form />
+      </ThemeProvider>
     </StateXProvider>
   ) : null;
 }
